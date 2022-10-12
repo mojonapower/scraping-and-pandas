d3b79540e45dafc6d2102635a1f3bc48a4b182f9
mojonapower <maca95@gmail.com>
2022-10-12 15:57:26 +0000

    nueva casa

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from scraping import moreexchange
+from scraping import cambiossuiza
+
 app = Flask(__name__)
 from flask_cors import CORS
 
@@ -11,8 +13,14 @@ def scrap():
     more=moreexchange()
     return jsonify({
         "moreexchange": str(more),
+        "casa1": "another value"
+  })
+@app.route("/casa2", methods=['GET'])
+def scrap2():
+    cambios=cambiossuiza()
+    return jsonify({
+        "cambiossuiza": str(cambios),
         "casa2": "another value"
     })
-
 
 app.run(host='0.0.0.0', port='3000', debug=True)
--- scraping.py
+++ scraping.py
@@ -3,8 +3,16 @@ import requests
 import pandas as pd
 
 def moreexchange():
-    page = requests.get("""https://www.moreexchange.cl/tipo-de-cambio-de-divisas-more-exchange/%27""")
+    page = requests.get("""https://www.moreexchange.cl/tipo-de-cambio-de-divisas-more-exchange/""")
     soup = BeautifulSoup (page.text, 'html.parser')
     tables = soup.find_all('table')
     return tables[0]
+
+def cambiossuiza():
+    page = requests.get("""https://www.desarrollaonline.com/guinazu/index.php?suc=1""")
+    soup = BeautifulSoup (page.text, 'html.parser')
+    tables = soup.find_all('table')
+    print(tables)
+    return tables[0]
+
 
